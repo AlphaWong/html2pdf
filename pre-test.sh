b5ee2316729fee0a71a7d498bb10266fcd130a90
Alpha <AlphaWong@users.noreply.github.com>
2018-09-12 23:00:51 +0800

    Update pre-test.sh

--- pre-test.sh
+++ pre-test.sh
@@ -3,7 +3,7 @@
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories && \
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories && \
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && \
-apk add --no-cache wkhtmltopdf font-noto curl fontconfig make git && \
+apk add --no-cache wkhtmltopdf font-noto curl fontconfig make git alpine-sdk && \
 curl -O https://noto-website.storage.googleapis.com/pkgs/Noto-unhinted.zip && \
 mkdir -p /usr/share/fonts/Noto-unhinted && \
 unzip Noto-unhinted.zip -d /usr/share/fonts/Noto-unhinted/ && \
