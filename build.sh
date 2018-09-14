49401d65fd250625452aa9a4f6959f0a999c27c2
Alpha <jhexe2002@gmail.com>
2018-09-14 14:55:03 +0800

    FIX: build sh

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh 
 
-apt-get update && apt-get install wget build-essential unzip -y && \
-wget -P /tmp/temp/ https://downloads.wkhtmltopdf.org/0.12/0.12.5/wkhtmltox_0.12.5-1.stretch_amd64.deb && \
-wget -P /tmp/temp/ https://noto-website.storage.googleapis.com/pkgs/Noto-unhinted.zip && \
-dpkg -i /tmp/temp/wkhtmltox_0.12.5-1.stretch_amd64.deb || true && \
-apt install -f -y && \
-dpkg -i /tmp/temp/wkhtmltox_0.12.5-1.stretch_amd64.deb && \
-mkdir -p /usr/share/fonts/Noto-unhinted && \
-unzip /tmp/temp/Noto-unhinted.zip -d /usr/share/fonts/Noto-unhinted/ && \
+apt-get -qq update
+apt-get -qq install wget build-essential unzip
+wget -q -P /tmp/temp/ https://downloads.wkhtmltopdf.org/0.12/0.12.5/wkhtmltox_0.12.5-1.stretch_amd64.deb
+wget -q -P /tmp/temp/ https://noto-website.storage.googleapis.com/pkgs/Noto-unhinted.zip
+dpkg -i /tmp/temp/wkhtmltox_0.12.5-1.stretch_amd64.deb > /dev/null
+apt -qq install -f -y
+dpkg -i /tmp/temp/wkhtmltox_0.12.5-1.stretch_amd64.deb > /dev/null
+mkdir -p /usr/share/fonts/Noto-unhinted
+unzip /tmp/temp/Noto-unhinted.zip -d /usr/share/fonts/Noto-unhinted/
 fc-cache -fv
